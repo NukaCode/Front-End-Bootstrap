b60165c61b2fabd036a6eee25f2625a7ee9b159d
stygiansabyss <stygian.warlock.v2@gmail.com>
2015-02-17 16:57:58 -0600

    Update 401.blade.php

--- src/views/errors/401.blade.php
+++ src/views/errors/401.blade.php
@@ -1,13 +1,1 @@
-<div class="row">
-	<div class="col-md-offset-4 col-md-4">
-		<div class="well text-center">
-			<div class="well-title">
-				401 Unauthorized Error
-			</div>
-			You are not authorized to view this page. <br />
-			@if (Session::has('errorMessage'))
-				Error Message: {{ Session::get('errorMessage') }}
-			@endif
-		</div>
-	</div>
-</div>
+@include('errors.layout', ['error' => 401, 'subTitle' => 'You are not authorized to view this page.'])
