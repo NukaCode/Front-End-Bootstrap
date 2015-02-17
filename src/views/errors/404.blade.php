a787daa5455d0c2432033cffd591e49170933658
stygiansabyss <stygian.warlock.v2@gmail.com>
2015-02-17 16:58:08 -0600

    Update 404.blade.php

--- src/views/errors/404.blade.php
+++ src/views/errors/404.blade.php
@@ -1,13 +1,1 @@
-<div class="row">
-	<div class="col-md-offset-4 col-md-4">
-		<div class="well text-center">
-			<div class="well-title">
-				404 File Not Found Error
-			</div>
-			Sorry, the page you are looking for could not be found. <br />
-			@if (Session::has('errorMessage'))
-				Error Message: {{ Session::get('errorMessage') }}
-			@endif
-		</div>
-	</div>
-</div>
+@include('errors.layout', ['error' => 404, 'subTitle' => 'The page you requested could not be found.'])
